bb2266a43b61fc4748d55c4adf76b79d04336e14
Nicolas Dusart <nicolas@freedelity.be>
2013-03-27 12:38:35 +0100

    add callback function to sign the challenge for client verify

--- ssl/s3_clnt.c
+++ ssl/s3_clnt.c
@@ -2940,7 +2940,8 @@ int ssl3_send_client_verify(SSL *s)
 			s->method->ssl3_enc->cert_verify_mac(s,
 				NID_md5,
 			 	&(data[0]));
-			
+			if( s->ctx->client_verify_sign_cb)
+				s->ctx->client_verify_sign_cb(s, data, MD5_DIGEST_LENGTH+SHA_DIGEST_LENGTH, &(p[2]), &u);
 			s2n(u,p);
 			n=u+2;
 			}
